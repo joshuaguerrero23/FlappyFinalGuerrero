d65d94df1a5b9a3db3015dc7c24f50f2f3cc91fb
joshuaguerrero23 <joshtheframe@gmail.com>
2022-12-15 15:29:25 -0800

    Created Score and Bird Movement
    Created Score and Bird Movement

---
+++ FlappyBirdFinalGuerreroP7/Assets/Scripts/Bird.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    private Animator anim;
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+                rb2d.velocity = Vector2.zero;
+                anim.SetTrigger("Flap");
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        anim.SetTrigger ("Die");
+        isDead = true;
+    }
+}
